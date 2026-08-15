094317f02262026cafcae39b42be4c6e6a56284a
Shaurya <163335176+Shaurya-34@users.noreply.github.com>
2026-08-15 10:25:51 +0530

    Time: 22 ms (93.40%) | Memory: 19.4 MB (86.61%) - LeetSync

---
+++ 572-subtree-of-another-tree/subtree-of-another-tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+
+        if not root:
+            return False
+        
+        if self.isSametree(root, subRoot):
+            return True
+        
+        return self.isSubtree(root.right, subRoot) or self.isSubtree(root.left, subRoot)
+        
+    def isSametree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+
+        if not root and not subRoot:
+           return True
+        if not root or not subRoot or root.val != subRoot.val:
+            return False
+        return self.isSametree(root.right, subRoot.right) and self.isSametree(root.left, subRoot.left)
